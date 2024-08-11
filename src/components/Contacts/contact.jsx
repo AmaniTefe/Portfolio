09c9723d90b2733de44badc4652afce7e5f78102
Amanuel Tefera <amani123tefe@gmail.com>
2024-08-11 11:37:21 +0300

    version 2 Done

--- src/components/Contacts/contact.jsx
+++ src/components/Contacts/contact.jsx
@@ -2,7 +2,6 @@ import { Box, Typography, Button, IconButton, Stack } from "@mui/material";
 import React from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import TwitterIcon from "@mui/icons-material/Twitter";
 import EmailIcon from "@mui/icons-material/Email";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import { FaXTwitter } from "react-icons/fa6";
